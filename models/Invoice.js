e4d98374d36d97e16a81f7890d690f79c0fd92f3
jddev <hsd.salaheddine@gmail.com>
2023-08-16 14:38:51 +0100

    create invoice model

--- models/Invoice.js
+++ models/Invoice.js
@@ -1,7 +1,24 @@
 // Dependencies
 const mongoose = require("mongoose");
 
-const invoiceSchema = new mongoose.Schema({}, { timestamps: true });
+const invoiceSchema = new mongoose.Schema(
+    {
+        invoiceNumber: {
+            type: Number,
+            required: [true, "Invoice number is required"],
+        },
+        invoiceDate: {
+            type: Date,
+            required: [true, "Invoice date is required"],
+            default: Date.now(),
+        },
+        invoiceAmount: {
+            type: Number,
+            required: [true, "Invoice amount is required"],
+        },
+    },
+    { timestamps: true }
+);
 
 const Invoice = mongoose.model("Invoice", invoiceSchema);
 
